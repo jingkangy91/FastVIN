8cb6259bdc555c18f10b39698f9b5026d01971c2
Jack YANG <jingkangy91@gmail.com>
2018-08-03 12:14:39 +0800

    Create img dir and download png.

--- get_vin_img.py
+++ get_vin_img.py
@@ -6,15 +6,20 @@ filename = 'vin_list.csv'
 df = pd.read_csv(filename)
 df.drop(df.index[-5:], inplace=True)
 
-base_url = "http://application.vidc.info"
-post_url = base_url + "/subpage/nonloginprint.aspx"
+# base_url = "http://application.vidc.info"
+# post_url = base_url + "/subpage/nonloginprint.aspx"
+
+begin_url = "http://application.vidc.info/Scripts/ImageHandler.ashx?vin="
+end_url = "&sess=no&type=download/Certificate.png"
 
 # for index, row in df.iterrows():
 for index, row in df[:1].iterrows():
-    payload = {
-        'TextBox_vin': row['VIN'],
-        'TextBox_jkzms': row['Import Certificate Number'],
-    }
+    url = begin_url + row['VIN'] + end_url
+
+    # payload = {
+    #     'TextBox_vin': row['VIN'],
+    #     'TextBox_jkzms': row['Import Certificate Number'],
+    # }
     headers = {
         'user-agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) \
@@ -22,9 +27,13 @@ for index, row in df[:1].iterrows():
             Chrome/67.0.3396.99 Safari/537.36'
     }
     # print(data)
-    sess = requests.Session()
-    req = sess.post(post_url, headers=headers, data=payload)
-    req.encoding = 'utf-8'
-    html = req.text
-    print(html)
+    # sess = requests.Session()
+    # req = sess.post(post_url, headers=headers, data=payload)
+    # req.encoding = 'utf-8'
+    # html = req.text
+    # print(html)
     # print(req.cookies.get_dict())
+
+    req = requests.get(url, stream=True)
+    with open('./img/cert.png', 'wb') as f:
+        f.write(req.content)
