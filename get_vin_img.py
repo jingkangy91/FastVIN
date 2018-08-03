414adb44d60276293cbeddcf094a76b7c8c805c4
Jack YANG <jingkangy91@gmail.com>
2018-08-03 12:06:58 +0800

    Try to use session.

--- get_vin_img.py
+++ get_vin_img.py
@@ -7,7 +7,7 @@ df = pd.read_csv(filename)
 df.drop(df.index[-5:], inplace=True)
 
 base_url = "http://application.vidc.info"
-post_url = base_url + "/subpage/nonloginprint.aspx?"
+post_url = base_url + "/subpage/nonloginprint.aspx"
 
 # for index, row in df.iterrows():
 for index, row in df[:1].iterrows():
@@ -15,8 +15,15 @@ for index, row in df[:1].iterrows():
         'TextBox_vin': row['VIN'],
         'TextBox_jkzms': row['Import Certificate Number'],
     }
+    headers = {
+        'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/67.0.3396.99 Safari/537.36'
+    }
     # print(data)
-    req = requests.post(post_url, data=payload)
+    sess = requests.Session()
+    req = sess.post(post_url, headers=headers, data=payload)
     req.encoding = 'utf-8'
     html = req.text
     print(html)
